dc0af89525ba2d87117dd62ad97037d90c3e3118
Benjamin M. Brown <benjaminbb@gmail.com>
2016-09-06 22:58:28 -0500

    skipping first and last cells

--- CellularAutomata.js
+++ CellularAutomata.js
@@ -1,21 +1,18 @@
 var CellularAutomata = function() {
     this.w = 20;
-    //this.cells = new Array(width / this.w);
-    this.cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0]
-    // for (var i = 0; i < this.cells.length; i++) {
-    //     this.cells[i] = 0;
-    // }
+    this.cells = [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0]
 
-  //  this.cells[this.cells.length / 2] = 1;
-   // this.generation = 0;
-//cell's state formula : CELL state at time t = f(CELL neighborhood at time t - 1)
+    //  this.cells[this.cells.length / 2] = 1;
+    this.generation = 0;
+    //cell's state formula : CELL state at time t = f(CELL neighborhood at time t - 1)
 
     this.display = function() {
-        for (var i = 0; i < this.cells.length; i++) {
+        var newCells = [];
+        for (var i = 1; i < this.cells.length - 1; i++) {
             if (this.cells[i] == 0) { fill(255) } else { fill(0) };
-            
+
             stroke(0);
-            rect(i*20,0,20,20);
+            rect(i * 20, 0, 20, 20);
 
 
         }
